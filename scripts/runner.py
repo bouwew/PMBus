f4c2352e41d81dc6a94358e61a15a1118f76f68b
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2020-05-13 16:15:38 +0200

    Update runner.py

--- scripts/runner.py
+++ scripts/runner.py
@@ -5,21 +5,29 @@ from pmbus import PMBus
 
 print("Initializing PMBUS... \n")
 
-DRQ = PMBus(0x12) #New pmbus object with device address 0x12
+HEP = PMBus(0x40) #New pmbus object with device address 0x40
 
 time.sleep(1)
 
-DRQ.setVinUVLimit(36.0)
+#HEP.setVinUVLimit(36.0)
 
 time.sleep(1)
 
 while True:
-    print("Tempurature: " + str(DRQ.getTempurature()))
-    print("Input Voltage: " + str(DRQ.getVoltageIn()))
-    print("Output Voltage: " + str(DRQ.getVoltageOut()))
-    print("Output Current: " + str(DRQ.getCurrent()))
-    print("Output Power: " + str(DRQ.getPowerOut(False)) + "\n\n") #False is caclulated from given values of current and voltage while True gets values from DRQ1250
-
+    print("Temperature: " + str(HEP.getTempurature()))
+    print("Input Voltage: " + str(HEP.getVoltageIn()))
+    print("Output Voltage: " + str(HEP.getVoltageOut()))
+    print("Output Current: " + str(HEP.getCurrent()))
+    print("Output Power: " + str(HEP.getPowerOut(False)) + "\n\n") #False is caclulated from given values of current and voltage while True gets values from DRQ1250
+    print("Status: " + str(HEP.getStatusSummary()) + "\n")
+    print("CURVE_CC: " + str(HEP.getCURVE_CC()) + "\n")
+    print("CURVE_CV: " + str(HEP.getCURVE_CV()) + "\n")
+    print("CURVE_FC: " + str(HEP.getCURVE_FV()) + "\n")
+    print("CURVE_TC: " + str(HEP.getCURVE_TC()) + "\n")
+    print("Charger Status: " + str(HEP.getCHGStatus()) + "\n")
+    print("Curve_Config: " + str(HEP.getCurveConfig()) + "\n")
+    print("System Status: " + str(HEP.getSystemStatus()) + "\n")
+    print("Systen Config: " + str(HEP.getSystemConfig()) + "\n")
     #DRQ.encodePMBus(34.0)
 
     time.sleep(5)
